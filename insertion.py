c86a8eaa32a8dc0b908274c61747718f2d1c0966
Adhil Rahiman M <adhilriju111@gmail.com>
2026-09-12 15:47:58 +0530

    new one

---
+++ insertion.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+a=Node(10)
+b=Node(20)
+c=Node(30)
+a.next=b
+b.next=c
+
+
+head =a
+temp=head
+while temp is not None:
+    print(temp.data)
+    temp=temp.next
+
+d=Node(5)
+d.next=head
+head=d
+temp=head
+print("after insertion")
+while temp is not None:
+    print(temp.data)
+    temp=temp.next
